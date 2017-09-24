d3bb94366f34c01a69c303311c2f6026a3180f65
Emil Koutanov <ekoutanov@gmail.com>
2017-09-24 21:58:09 +1000

    Increased coverage of high-water mark logic

--- integrated/src/test/java/com/obsidiandynamics/socketx/HighWaterMarkTest.java
+++ integrated/src/test/java/com/obsidiandynamics/socketx/HighWaterMarkTest.java
@@ -1,11 +1,14 @@
 package com.obsidiandynamics.socketx;
 
 import static junit.framework.TestCase.*;
+import static org.mockito.Mockito.*;
 
+import java.nio.*;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.*;
 
 import org.junit.*;
+import org.mockito.*;
 
 import com.obsidiandynamics.indigo.util.*;
 import com.obsidiandynamics.socketx.jetty.*;
@@ -14,23 +17,42 @@ import com.obsidiandynamics.socketx.undertow.*;
 import com.obsidiandynamics.socketx.util.*;
 
 public final class HighWaterMarkTest extends BaseClientServerTest {
+  private static final boolean TEXT = true;
+  private static final boolean BINARY = false;
+  
   @Test
-  public void testJtJt() throws Exception {
-    test(JettyServer.factory(), JettyClient.factory());
+  public void testJtJtText() throws Exception {
+    test(JettyServer.factory(), JettyClient.factory(), TEXT);
+  }
+  
+  @Test
+  public void testJtJtBinary() throws Exception {
+    test(JettyServer.factory(), JettyClient.factory(), BINARY);
   }
 
   @Test
-  public void testUtUt() throws Exception {
-    test(UndertowServer.factory(), UndertowClient.factory());
+  public void testUtUtText() throws Exception {
+    test(UndertowServer.factory(), UndertowClient.factory(), TEXT);
   }
 
   @Test
-  public void testNtUt() throws Exception {
-    test(NettyServer.factory(), UndertowClient.factory());
+  public void testUtUtBinary() throws Exception {
+    test(UndertowServer.factory(), UndertowClient.factory(), BINARY);
+  }
+
+  @Test
+  public void testNtUtText() throws Exception {
+    test(NettyServer.factory(), UndertowClient.factory(), TEXT);
+  }
+
+  @Test
+  public void testNtUtBinary() throws Exception {
+    test(NettyServer.factory(), UndertowClient.factory(), BINARY);
   }
 
   private void test(XServerFactory<? extends XEndpoint> serverFactory,
-                    XClientFactory<? extends XEndpoint> clientFactory) throws Exception {
+                    XClientFactory<? extends XEndpoint> clientFactory,
+                    boolean text) throws Exception {
     final int highWaterMark = 1;
     final XServerConfig serverConfig = getDefaultServerConfig(false)
         .withScanInterval(1)
@@ -45,21 +67,31 @@ public final class HighWaterMarkTest extends BaseClientServerTest {
     final CyclicBarrier receiveStarted = new CyclicBarrier(2);
     final CyclicBarrier receiveComplete = new CyclicBarrier(2);
     final AtomicBoolean complete = new AtomicBoolean();
+    final Runnable receivedHandler = () -> {
+      if (! complete.get()) {
+        TestSupport.await(receiveStarted);
+        TestSupport.await(receiveComplete);
+      }
+    };
     final XEndpointListener<XEndpoint> clientListener = new XEndpointLambdaListener<>()
-        .onText((endpoint, message) -> {
-          if (! complete.get()) {
-            TestSupport.await(receiveStarted);
-            TestSupport.await(receiveComplete);
-          }
-        });
+        .onText((endpoint, message) -> receivedHandler.run())
+        .onBinary((endpoint, message) -> receivedHandler.run());
     openClientEndpoint(false, serverConfig.port, clientListener);
     
     SocketUtils.await().untilTrue(() -> ! server.getEndpointManager().getEndpoints().isEmpty());
 
     final int messages = highWaterMark * 100;
+    final XSendCallback callback = Mockito.mock(XSendCallback.class);
+    final int payloadLength = 8192;
+    final String textPayload = BinaryUtils.randomHexString(payloadLength);
+    final byte[] binaryPayload = BinaryUtils.randomBytes(payloadLength);
     for (XEndpoint endpoint : server.getEndpointManager().getEndpoints()) {
       for (int i = 0; i < messages; i++) {
-        endpoint.send("test", null);
+        if (text) {
+          endpoint.send(textPayload, i % 2 == 0 ? callback : null);
+        } else {
+          endpoint.send(ByteBuffer.wrap(binaryPayload), i % 2 == 0 ? callback : null);
+        }
         final long backlog = endpoint.getBacklog();
         if (backlog > highWaterMark) {
           TestSupport.await(receiveStarted);
@@ -76,5 +108,9 @@ public final class HighWaterMarkTest extends BaseClientServerTest {
         TestSupport.await(receiveComplete);
       }
     }
+    
+    SocketUtils.await().until(() -> {
+      verify(callback, atLeastOnce()).onSkip(any(XEndpoint.class));
+    });
   }
 }
